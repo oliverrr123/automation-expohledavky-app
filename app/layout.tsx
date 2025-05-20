936d6a0e793804d61fee8b2ff09ea987c4a39664
oliverrr123 <oliver.cingl@gmail.com>
2025-05-20 18:25:52 +0200

    feat: add GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -149,6 +149,15 @@ export default function RootLayout({
   return (
     <html lang={meta.language || ''} className={`scroll-smooth ${montserrat.variable}`}>
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MGSW4CZ');`
+        }} />
+        {/* End Google Tag Manager */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/* Recommended meta tags for SEO */}
         <meta httpEquiv="Content-Language" content={serverLocale} />
@@ -202,6 +211,16 @@ export default function RootLayout({
         )}
       </head>
       <body className={montserrat.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MGSW4CZ"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <Analytics />
         <SpeedInsights />
